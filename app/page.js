2e38db6f07b20387a35c1d93e8a85358c0766af1
sawitrees997-coder <sawitree.s997@gmail.com>
2026-03-05 09:08:14 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div style={{ padding: 40 }}>
-      <h1>Stock System พร้อมใช้งานแล้ว 🚀</h1>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        🎉 Tailwind ทำงานแล้ว!
+      </h1>
     </div>
   );
 }
