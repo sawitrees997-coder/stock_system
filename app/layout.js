b9c2933ab51418be715b91aa7f00de4df3acbbe8
sawitrees997-coder <sawitree.s997@gmail.com>
2026-03-05 08:46:37 +0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Stock System",
+  description: "Simple stock system",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
